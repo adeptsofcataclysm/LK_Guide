2298e90db22623835728bf5c3e13e552e57ee64e
drcrazy <hcb@wowhcb.ru>
2023-07-30 20:19:22 +0300

    Update site title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ICC Guide',
+  title: 'Gentle Guide',
   tagline: 'Moar wipes!!',
   favicon: 'img/favicon.ico',
 
