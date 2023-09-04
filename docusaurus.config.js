c0c2e8a326df8a63a2d2ae0c9a05f21bbb615dcc
drcrazy <hcb@wowhcb.ru>
2023-09-04 16:14:44 +0300

    Update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,33 +97,20 @@ const config = {
             ],
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://adeptsofcataclysm.github.io/LK_Guide/',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/NPSjpTeWwt',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Gentlemanchik. Built with Docusaurus.`,
       },
       prism: {
         theme: darkCodeTheme,
